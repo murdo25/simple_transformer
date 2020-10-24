9306ecf8b5e40c622f7e0f32ea298635f16b5c64
murdo25 <ben.murdoch.94@gmail.com>
2020-10-24 15:32:51 -0600

    simple read to test the character set vocab

--- data/read.py
+++ data/read.py
@@ -1,11 +1,21 @@
 
-
-
 file1 = open('mini_train_set.txt', 'r') 
 Lines = file1.readlines() 
-  
+
+
+characters = {}
+
+
 count = 0
 # Strips the newline character 
 for line in Lines: 
-    print("Line {}: {}".format(count, line.strip()))
+    line = line.strip()
+    print("Line {}: {}".format(count, line))
     count += 1
+    for c in line:
+        if c in characters:
+            characters[c] += 1
+        else:
+            characters[c] = 0
+    print(characters)
+
